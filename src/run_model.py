81c1764a5d009b5a88577b196a9e6fb94ab758be
Yograj Mahawar <89453936+yrajm1997@users.noreply.github.com>
2024-10-14 12:04:32 +0000

    challenge done

--- src/run_model.py
+++ src/run_model.py
@@ -1,8 +1,17 @@
 import os
 import pickle
 
+import os
+import sys
+from pathlib import Path
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
 def load_model():
-    model_path = '/workspaces/mlops-churn-prediction/models/churn_model.pkl' 
+    #model_path = '/workspaces/mlops-churn-prediction/models/churn_model.pkl'
+    #model_path = '/workspaces/mlops-customer-churn-prediction-using-ml-yrajm1997/models/churn_model.pkl' 
+    model_path = 'models/churn_model.pkl'
     with open(model_path, 'rb') as model_file:
         model = pickle.load(model_file)
     return model
